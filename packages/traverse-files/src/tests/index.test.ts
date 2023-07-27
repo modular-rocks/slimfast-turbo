086e6bdbe1825cd25158d63eee3891c15644d386
Antony Faris <acp325@gmail.com>
2023-07-27 10:18:03 -0500

    types refactor

--- packages/traverse-files/src/tests/index.test.ts
+++ packages/traverse-files/src/tests/index.test.ts
@@ -1,96 +1,119 @@
-import { 
-  resolve, 
-  collect, 
-  traverse, 
-  removeTests, 
-  removeExtensions, 
+import {
+  resolve,
+  collect,
+  traverse,
+  removeTests,
+  removeExtensions,
   removeIgnoredExtensions,
   readDirectory,
-  readJSONFile
-} from '../index'
+  readJSONFile,
+} from '../index';
 
-describe("removeExtensions works as intended", () => {
-  test("", () => {
-    const extensions: string[] = ['js', 'ts'] 
+import type { Directory, Options, RandomObject } from '../types';
+
+describe('removeExtensions works as intended', () => {
+  test('', () => {
+    const extensions = ['js', 'ts'];
     expect(removeExtensions(extensions)('hello-world.js')).toEqual(true);
     expect(removeExtensions(extensions)('hello-world.gs')).toEqual(false);
     expect(removeExtensions(extensions)('hello-world.ts')).toEqual(true);
   });
 });
 
-describe("removeIgnoredExtensions works as intended", () => {
-  test("", () => {
-    const ignoredExtensions = ['.d.ts', '.d.tsx']
+describe('removeIgnoredExtensions works as intended', () => {
+  test('', () => {
+    const ignoredExtensions = ['.d.ts', '.d.tsx'];
     expect(removeIgnoredExtensions(ignoredExtensions)('hello-world.d.ts')).toEqual(false);
     expect(removeIgnoredExtensions(ignoredExtensions)('hello-world.d.gs')).toEqual(true);
     expect(removeIgnoredExtensions(ignoredExtensions)('hello-world.ts')).toEqual(true);
   });
 });
 
-describe("removeTests works as intended", () => {
-  test("", () => {
+describe('removeTests works as intended', () => {
+  test('', () => {
     expect(removeTests('hello-world.test.ts')).toEqual(false);
     expect(removeTests('hello-world.ts')).toEqual(true);
   });
 });
 
-describe("collect works as intended", () => {
-  test("", () => {
-    const fullpath: string = resolve(__dirname, './test-directory')
-    const extensions = ['js', 'ts', 'tsx', 'jsx']
-    const ignoredExtensions = ['.d.ts', '.d.tsx']
-    const ignoreTests = true
+describe('collect works as intended', () => {
+  test('', () => {
+    const fullpath: string = resolve(__dirname, './test-directory');
+    const extensions = ['js', 'ts', 'tsx', 'jsx'];
+    const ignoredExtensions = ['.d.ts', '.d.tsx'];
+    const ignoreTests = true;
 
-    let collected = collect(fullpath, extensions, ignoredExtensions, ignoreTests)
-    collected = collected.map((path: string) => path.replace(process.cwd(), ''))
+    let collected: string[] = collect(fullpath, extensions, ignoredExtensions, ignoreTests);
+    collected = collected.map((path: string) => path.replace(process.cwd(), ''));
 
-    
-    const expected = ["/src/tests/test-directory/nested/one.js", "/src/tests/test-directory/nested/three.js", "/src/tests/test-directory/nested/two.js", "/src/tests/test-directory/one.js", "/src/tests/test-directory/three.js", "/src/tests/test-directory/two.js"]
-
-    expect(collected).toEqual(expected);
-  });
-});
-
-describe("traverse works as intended", () => {
-  test("", () => {
-    const fullpath: string = resolve(__dirname, './test-directory')
-
-    let collected = traverse(fullpath, [])
-    collected = collected.map((path: string) => path.replace(process.cwd(), ''))
-
-    const expected = ["/src/tests/test-directory/nested/one.js", "/src/tests/test-directory/nested/three.js", "/src/tests/test-directory/nested/two.js", "/src/tests/test-directory/one-declaration.d.ts", "/src/tests/test-directory/one-test.test.js", "/src/tests/test-directory/one.js", "/src/tests/test-directory/three.js", "/src/tests/test-directory/two.js"]
+    const expected = [
+      '/src/tests/test-directory/nested/one.js',
+      '/src/tests/test-directory/nested/three.js',
+      '/src/tests/test-directory/nested/two.js',
+      '/src/tests/test-directory/one.js',
+      '/src/tests/test-directory/three.js',
+      '/src/tests/test-directory/two.js',
+    ];
 
     expect(collected).toEqual(expected);
   });
 });
-describe("readDirectory works as intended", () => {
-  test("", () => {
-    const src: string = resolve(__dirname, './test-directory')
-    const extensions = ['js', 'ts', 'tsx', 'jsx']
-    const ignoredFiles = ['.d.ts', '.d.tsx']
 
-    const opts = {
+describe('traverse works as intended', () => {
+  test('', () => {
+    const fullpath: string = resolve(__dirname, './test-directory');
+
+    let collected: string[] = traverse(fullpath, []);
+    collected = collected.map((path) => path.replace(process.cwd(), ''));
+
+    const expected = [
+      '/src/tests/test-directory/nested/one.js',
+      '/src/tests/test-directory/nested/three.js',
+      '/src/tests/test-directory/nested/two.js',
+      '/src/tests/test-directory/one-declaration.d.ts',
+      '/src/tests/test-directory/one-test.test.js',
+      '/src/tests/test-directory/one.js',
+      '/src/tests/test-directory/three.js',
+      '/src/tests/test-directory/two.js',
+    ];
+
+    expect(collected).toEqual(expected);
+  });
+});
+describe('readDirectory works as intended', () => {
+  test('', () => {
+    const src: string = resolve(__dirname, './test-directory');
+    const extensions = ['js', 'ts', 'tsx', 'jsx'];
+    const ignoredFiles = ['.d.ts', '.d.tsx'];
+
+    const opts: Options = {
       src,
       extensions,
-      ignoredFiles
-    }
+      ignoredFiles,
+    };
 
-    let collected: Directory = readDirectory(opts)
-    collected.forEach((file: CollectedFile) => file[0] = file[0].replace(process.cwd(), ''))
+    let collected: Directory = readDirectory(opts);
+    collected.forEach((file) => (file[0] = file[0].replace(process.cwd(), '')));
 
-    const expected = [["/src/tests/test-directory/nested/one.js", ""], ["/src/tests/test-directory/nested/three.js", ""], ["/src/tests/test-directory/nested/two.js", ""], ["/src/tests/test-directory/one.js", ""], ["/src/tests/test-directory/three.js", ""], ["/src/tests/test-directory/two.js", ""]]
+    const expected = [
+      ['/src/tests/test-directory/nested/one.js', ''],
+      ['/src/tests/test-directory/nested/three.js', ''],
+      ['/src/tests/test-directory/nested/two.js', ''],
+      ['/src/tests/test-directory/one.js', ''],
+      ['/src/tests/test-directory/three.js', ''],
+      ['/src/tests/test-directory/two.js', ''],
+    ];
 
     expect(collected).toEqual(expected);
   });
 });
 
+describe('readJSONFile works as intended', () => {
+  test('', () => {
+    const packagePath: string = resolve(__dirname, './test-package.json');
+    const contents: RandomObject = readJSONFile(packagePath);
 
-describe("readJSONFile works as intended", () => {
-  test("", () => {
-    const packagePath: string = resolve(__dirname, './test-package.json')
-    const contents: RandomObject = readJSONFile(packagePath)
-
-    const expected = {}
+    const expected = {};
 
     expect(expected).toEqual(contents);
   });
