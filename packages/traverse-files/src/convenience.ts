571d1bd6043204571f59ab9ed1f381566e10d2e1
Antony Faris <acp325@gmail.com>
2023-07-28 18:25:39 -0500

    enhance file path handling in traverse-files

--- packages/traverse-files/src/convenience.ts
+++ packages/traverse-files/src/convenience.ts
@@ -1,7 +1,49 @@
-import path from 'path';
+import { resolve as pathResolve, posix, sep } from 'path';
 import { readFileSync, statSync, readdirSync } from 'fs';
 
-export const read = (path: string): string => readFileSync(path).toString();
-export const resolve = (dirname: string, filename: string): string => path.resolve(dirname, filename);
-export const readFilesInDirectory = (directoryPath: string): string[] => readdirSync(directoryPath);
+/**
+ * Reads the content of a file and returns it as a string.
+ * @param filePath - The path of the file to be read.
+ * @returns The content of the file as a string.
+ */
+export const read = (filePath: string): string => readFileSync(filePath).toString();
+
+/**
+ * Resolves a filename relative to a directory to get the absolute path.
+ * The function normalizes the directory and filename to POSIX style
+ * paths and then resolves the absolute path.
+ * @param dirname - The directory name from which the filename is resolved.
+ * @param filename - The filename to be resolved.
+ * @returns The absolute path of the resolved filename.
+ */
+export const resolve = (dirname: string, filename: string): string => {
+  dirname = normalizePathToPosix(dirname);
+  filename = normalizePathToPosix(filename);
+
+  return normalizePathToPosix(pathResolve(dirname, filename));
+};
+
+/**
+ * Reads all the filenames in a given directory and returns them as an array of strings.
+ * @param directoryPath - The path of the directory to read.
+ * @returns An array of strings representing the filenames in the directory.
+ */
+export const readFilesInDirectory = (directoryPath: string): string[] => {
+  const posixDirectoryPath = posix.normalize(directoryPath);
+  return readdirSync(posixDirectoryPath);
+};
+
+/**
+ * Checks if a given absolute path is a directory.
+ * @param absolutePath - The absolute path to check.
+ * @returns `true` if the path is a directory, otherwise `false`.
+ */
 export const isDirectory = (absolutePath: string): boolean => statSync(absolutePath).isDirectory();
+
+/**
+ * Converts a directory path to a POSIX-style path by replacing backslashes with forward slashes.
+ * This function is used to ensure path consistency, especially when dealing with different operating systems.
+ * @param path - The directory path to be converted.
+ * @returns The directory path with POSIX-style separators.
+ */
+export const normalizePathToPosix = (path: string): string => path.split(sep).join(posix.sep);
--- packages/traverse-files/src/index.ts
+++ packages/traverse-files/src/index.ts
@@ -1,30 +1,39 @@
-import path from 'path';
+import { extname, basename, posix } from 'path';
 
 import { read, resolve, readFilesInDirectory, isDirectory } from './convenience';
 
 import type { Directory, Options, RandomObject } from './types';
 
 const removeExtensions = (extensions: string[]) => (fullpath: string) => {
-  return extensions.includes(path.extname(fullpath).replace('.', ''));
+  return extensions.includes(extname(fullpath).replace('.', ''));
 };
 
 const removeIgnoredExtensions = (ignoredExtensions: string[]) => (fullpath: string) => {
   return !ignoredExtensions.map((ex: string) => fullpath.includes(ex)).filter(Boolean)[0];
 };
 
-const removeTests = (fullpath: string) => !path.basename(fullpath).match(/test/);
+const removeTests = (fullpath: string) => !basename(fullpath).match(/test/);
 
-function traverse(directory: string, files: string[]) {
+function traverse(directory: string, files: string[]): string[] {
+  const accumulatedFiles: string[] = [...files];
+
   readFilesInDirectory(directory).forEach((filePath: string) => {
-    const absolutePath: string = path.join(directory, filePath);
-    return isDirectory(absolutePath) ? traverse(absolutePath, files) : files.push(absolutePath);
+    const absolutePath: string = posix.join(directory, filePath);
+    if (isDirectory(absolutePath)) {
+      // If it's a directory, call traverse recursively and update the accumulatedFiles
+      accumulatedFiles.push(...traverse(absolutePath, []));
+    } else {
+      // If it's a file, add the absolute path to the accumulatedFiles
+      accumulatedFiles.push(absolutePath);
+    }
   });
-  return files;
+
+  return accumulatedFiles; // Return the new array with all file paths
 }
 
 const collect = (path: string, extensions: string[], ignoredExtensions: string[], ignoreTests: Boolean) => {
-  let files: string[] = [];
-  traverse(path, files);
+  const files: string[] = traverse(path, []);
+
   let output = files.filter(removeExtensions(extensions)).filter(removeIgnoredExtensions(ignoredExtensions));
 
   if (ignoreTests) {
