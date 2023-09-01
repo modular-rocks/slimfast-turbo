2e7be3f251b4e3cc9cf21d30f1cb3660b3e71a2d
Antony Faris <acp325@gmail.com>
2023-09-01 09:15:51 -0500

    [workspace]: Refactor make-directory function and add tests (#42)

--- packages/workspace/src/workspace/codebase/make-directory/index.test.ts
+++ packages/workspace/src/workspace/codebase/make-directory/index.test.ts
@@ -6,27 +6,50 @@ import { Codebase } from '..';
 
 import type { CodebaseOpts } from '../../../types';
 
-describe('Make Directory', () => {
-  test('Everything works', async () => {
-    const files: [string, string][] = [1, 2, 3].map((x: number) => [
-      normalize(`/home/projects/project/path${x}.ts`),
-      '',
-    ]);
-    const pipeline: Function[] = [];
+describe('"makeDirectory" utility', () => {
+  const opts: CodebaseOpts = {
+    pipeline: [],
+    files: [],
+    src: normalize('/home/projects/project/'),
+    extensions: [],
+    ignoredFiles: [],
+    ignoredImports: [],
+    packageContents: {},
+  };
 
-    const opts: CodebaseOpts = {
-      pipeline,
-      files,
-      src: normalize('/home/projects/project/'),
-      extensions: [],
-      ignoredFiles: [],
-      ignoredImports: [],
-      packageContents: {},
-    };
+  test('Should restructure a file named other than "index"', () => {
+    const filePath = normalize('/home/projects/project/path1.ts');
+    const files: [string, string][] = [[filePath, '']];
+    const codebase = new Codebase({ ...opts, files });
 
-    const codebase = new Codebase(opts);
     const file = codebase.extractFiles()[0];
     const newSrc = makeDirectory(codebase, file);
+
     expect(normalize(newSrc)).toBe(normalize('/project/path1/index.ts'));
+    expect(codebase.files[normalize('/project/path1.ts')]).toBeUndefined();
+  });
+
+  test('Should leave a file named "index" unchanged', () => {
+    const filePath = normalize('/home/projects/project/index.ts');
+    const files: [string, string][] = [[filePath, '']];
+    const codebase = new Codebase({ ...opts, files });
+
+    const file = codebase.extractFiles()[0];
+    const newSrc = makeDirectory(codebase, file);
+
+    expect(normalize(newSrc)).toBe(normalize('/project/index.ts'));
+    expect(codebase.files[normalize('/project/index.ts')]).toBeDefined();
+  });
+
+  test('Should handle unusual file names correctly', () => {
+    const filePath = normalize('/home/projects/project/.file.ts');
+    const files: [string, string][] = [[filePath, '']];
+    const codebase = new Codebase({ ...opts, files });
+
+    const file = codebase.extractFiles()[0];
+    const newSrc = makeDirectory(codebase, file);
+
+    expect(normalize(newSrc)).toBe(normalize('/project/.file/index.ts'));
+    expect(codebase.files[normalize('/project/.file.ts')]).toBeUndefined();
   });
 });
