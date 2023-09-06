f2cd92cf2ac340615cde7d884c74ae3275372b43
Antony Faris <acp325@gmail.com>
2023-09-06 17:52:51 -0500

    [workspace-node]: Add JSDocs (#50)

---
+++ .changeset/modern-bees-pump.md
@@ -0,0 +1,5 @@
+---
+'@modular-rocks/workspace-node': patch
+---
+
+Internal: Added JSDocs to the `workspace-node` package for better code documentation.
--- packages/workspace-node/src/workspace/codebase/file/print/index.ts
+++ packages/workspace-node/src/workspace/codebase/file/print/index.ts
@@ -1,5 +1,20 @@
 import type { Options, types } from 'recast';
 import { print as recastPrinter } from 'recast';
 
+/**
+ * Prints the given AST node using the provided options.
+ *
+ * @param ast - The AST node to be printed.
+ * @param opts - recast options https://github.com/benjamn/recast/blob/master/lib/options.ts
+ * @returns An object containing:
+ *  - `code`: A string representation of the AST node.
+ *  - `map`: An optional source map if the `sourceMapName` option is set.
+ *  - `toString()`: A method that returns the code as a string.
+ *
+ * @example
+ * const printedResult = print(astNode, options);
+ * console.log(printedResult.code);
+ * // Outputs the printed code based on the AST node and options.
+ */
 export const print = (ast: types.ASTNode, opts?: Options) =>
   recastPrinter(ast, opts);
