92af53cda797246af195bed5f5913188c67617af
Antony Faris <acp325@gmail.com>
2023-11-14 20:20:26 -0500

    [workspace-node]: Fix deprecation warning

--- packages/workspace-node/src/workspace/codebase/file/print/index.ts
+++ packages/workspace-node/src/workspace/codebase/file/print/index.ts
@@ -10,12 +10,16 @@ import type { Options, types } from 'recast';
  * @returns An object containing:
  *  - `code`: A string representation of the AST node.
  *  - `map`: An optional source map if the `sourceMapName` option is set.
- *  - `toString()`: A method that returns the code as a string.
  *
  * @example
  * const printedResult = print(astNode, options);
  * console.log(printedResult.code);
  * // Outputs the printed code based on the AST node and options.
  */
-export const print = (ast: types.ASTNode, opts?: Options) =>
-  recastPrinter(ast, opts);
+export const print = (ast: types.ASTNode, opts?: Options) => {
+  const result = recastPrinter(ast, opts);
+  return {
+    code: result.code,
+    map: result.map,
+  };
+};
