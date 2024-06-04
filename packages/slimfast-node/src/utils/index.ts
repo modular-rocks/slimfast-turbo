8922e02335127541eb34f6850202d1808d8cff50
Antony Faris <acp325@gmail.com>
2024-06-04 02:13:07 +0000

    Export utils

--- packages/slimfast-node/src/utils/index.ts
+++ packages/slimfast-node/src/utils/index.ts
@@ -60,3 +60,50 @@ export function uniqueImmutable<T>(arr: T[]): T[] {
 
   return unique(newArr);
 }
+
+export { combineImports } from '../slimfast/pipeline/build/builder/combine-imports';
+export { generateImportDeclaration } from '../slimfast/pipeline/build/builder/import-statement';
+export { generateJSXElement } from '../slimfast/pipeline/build/builder/replace/jsx-function';
+export { generateFunction } from '../slimfast/pipeline/build/builder/replace/normal-function';
+export { replace } from '../slimfast/pipeline/build/builder/replace';
+export { hasAwait } from '../slimfast/pipeline/build/builder/wrap/has-await';
+export { generateExportedJSXComponent } from '../slimfast/pipeline/build/builder/wrap/jsx-function';
+export { generateExportedFunction } from '../slimfast/pipeline/build/builder/wrap/normal-function';
+export { wrap } from '../slimfast/pipeline/build/builder/wrap';
+// double check
+export { builder } from '../slimfast/pipeline/build/builder';
+// double check
+export { build } from '../slimfast/pipeline/build';
+// double check
+export { extract } from '../slimfast/pipeline/extract';
+
+export { defaultFunctionNameGenerator } from '../slimfast/pipeline/name/default-function-name-generator';
+// double check
+export { name } from '../slimfast/pipeline/name';
+
+// double check
+export { ExpressionVisitor } from '../slimfast/visitors/expression';
+
+// constraints
+export { containsIdentifiersInOtherScopes } from '../slimfast/visitors/utils/contraints/contains-identifiers-in-other-scopes';
+export { hasAssignmentExpression } from '../slimfast/visitors/utils/contraints/has-assignment-expression';
+export { hasBlocklistedIdentifiers } from '../slimfast/visitors/utils/contraints/has-blocklisted-identifiers';
+export { hasReturnStatement } from '../slimfast/visitors/utils/contraints/has-return-statement';
+export { hasVariableDeclarator } from '../slimfast/visitors/utils/contraints/has-variable-declarator';
+export { identifiersNotWithinRange } from '../slimfast/visitors/utils/contraints/identifiers-not-within-range';
+export { identifiersWithinRange } from '../slimfast/visitors/utils/contraints/identifiers-within-range';
+export { isAFunction } from '../slimfast/visitors/utils/contraints/is-a-function';
+export { isCallExpression } from '../slimfast/visitors/utils/contraints/is-call-expression';
+export { removesTooMuch } from '../slimfast/visitors/utils/contraints/removes-too-much';
+export { shouldIgnore } from '../slimfast/visitors/utils/contraints/should-ignore';
+export { tooSmall } from '../slimfast/visitors/utils/contraints/too-small';
+
+// others
+export { extractIdentifiers } from '../slimfast/visitors/utils/extract-identifiers';
+export { notInExtracted } from '../slimfast/visitors/utils/not-in-extracted';
+// double check
+export { parser } from '../slimfast/visitors/utils/parser';
+export { rejectParentsWithTypes } from '../slimfast/visitors/utils/reject-parents-with-types';
+
+// double check
+export { Visitor } from '../slimfast/visitors/visitor';
